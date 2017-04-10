fa9a2e9b932bc73bd4ebaca284dd91e851d3ada6
Lelthar <geraldma1998@gmail.com>
2017-04-09 19:59:54 -0600

    Commit Generar numeros

--- KakuroPrograAnalisis/src/kakuroprograanalisis/KakuroPrograAnalisis.java
+++ KakuroPrograAnalisis/src/kakuroprograanalisis/KakuroPrograAnalisis.java
@@ -5,6 +5,8 @@
  */
 package kakuroprograanalisis;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author gerald
@@ -17,6 +19,21 @@ public class KakuroPrograAnalisis {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Que pasó?");
+        ArrayList<Integer> lista = new ArrayList<>();
+        generar(0,3,0,8,1,lista);
     }
-    
+    public void generar(int num,int cantidad,int k,int meta,int i,ArrayList lista){
+        if(k == cantidad && num == meta){
+            System.out.println(lista);
+        }else if(k == cantidad){
+            System.out.println("No hay");
+        }else{
+            for(int j = 0; j<10; j++){
+                if(num+j < meta & k+1 != cantidad | num+j == meta & k+1 == cantidad ){
+                    lista.add(j);
+                    generar(num+j,cantidad,k+1,meta,i+1,lista);
+                }
+            }
+        }
+    } 
 }
