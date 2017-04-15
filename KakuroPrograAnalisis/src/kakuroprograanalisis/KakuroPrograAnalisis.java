5b3bd9beec5abd3251298a7f605d48581c9aa79d
Lelthar <geraldma1998@gmail.com>
2017-04-15 14:32:25 -0600

    prueba 2..

--- KakuroPrograAnalisis/src/kakuroprograanalisis/KakuroPrograAnalisis.java
+++ KakuroPrograAnalisis/src/kakuroprograanalisis/KakuroPrograAnalisis.java
@@ -20,8 +20,7 @@ public class KakuroPrograAnalisis {
      */
     public static void main(String[] args) {
         // TODO code application logic here   
-        //Verga, no se actualiza
-        
+        System.out.println("Que hago?");
     }
     
   
